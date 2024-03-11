fc7bb4fd91aa17e6287d25cbefaa7c5a634239e8
Wepplord <drefsantos1801@gmail.com>
2024-03-12 02:06:20 +0800

    Recommitted Alerts

---
+++ JCUBE_SE_PROJECT/Alert.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace JCUBE_SE_PROJECT
+{
+    partial class Alert
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Alert));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblItemCode = new System.Windows.Forms.Label();
+            this.MsgTxt = new System.Windows.Forms.Label();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnReOrder = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblItemCode);
+            this.panel1.Controls.Add(this.MsgTxt);
+            this.panel1.Controls.Add(this.btnCancel);
+            this.panel1.Controls.Add(this.btnReOrder);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(370, 75);
+            this.panel1.TabIndex = 0;
+            // 
+            // lblItemCode
+            // 
+            this.lblItemCode.AutoSize = true;
+            this.lblItemCode.Location = new System.Drawing.Point(340, 50);
+            this.lblItemCode.Name = "lblItemCode";
+            this.lblItemCode.Size = new System.Drawing.Size(25, 21);
+            this.lblItemCode.TabIndex = 7;
+            this.lblItemCode.Text = "lb";
+            this.lblItemCode.Visible = false;
+            // 
+            // MsgTxt
+            // 
+            this.MsgTxt.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MsgTxt.ForeColor = System.Drawing.Color.White;
+            this.MsgTxt.Location = new System.Drawing.Point(73, 4);
+            this.MsgTxt.Name = "MsgTxt";
+            this.MsgTxt.Size = new System.Drawing.Size(243, 54);
+            this.MsgTxt.TabIndex = 6;
+            this.MsgTxt.Text = "Message Text";
+            this.MsgTxt.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnCancel.BackgroundImage")));
+            this.btnCancel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnCancel.FlatAppearance.BorderSize = 0;
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancel.ForeColor = System.Drawing.Color.White;
+            this.btnCancel.Location = new System.Drawing.Point(322, 17);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(30, 30);
+            this.btnCancel.TabIndex = 5;
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnReOrder
+            // 
+            this.btnReOrder.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnReOrder.BackgroundImage")));
+            this.btnReOrder.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnReOrder.FlatAppearance.BorderSize = 0;
+            this.btnReOrder.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReOrder.ForeColor = System.Drawing.Color.White;
+            this.btnReOrder.Location = new System.Drawing.Point(6, 7);
+            this.btnReOrder.Name = "btnReOrder";
+            this.btnReOrder.Size = new System.Drawing.Size(61, 51);
+            this.btnReOrder.TabIndex = 4;
+            this.btnReOrder.UseVisualStyleBackColor = true;
+            // 
+            // Alert
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(370, 75);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Alert";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Alert";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnCancel;
+        public System.Windows.Forms.Label lblItemCode;
+        public System.Windows.Forms.Label MsgTxt;
+        public System.Windows.Forms.Button btnReOrder;
+    }
+}
